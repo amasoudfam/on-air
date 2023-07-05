b39d761d09d099a37417dd8624bc4adaf7639e37
mohammad serpoush <mohammad.serpush@gmail.com>
2023-07-05 19:39:41 +0330

    fix migrations

--- migrations/000003_users_unique_passenger_national_code.up.sql
+++ migrations/000003_users_unique_passenger_national_code.up.sql
@@ -1,3 +1,8 @@
 ALTER TABLE passengers
 ADD CONSTRAINT uk_passengers_user_id_national_code UNIQUE (user_id, national_code);
 
+ALTER TABLE users
+ADD CONSTRAINT uk_users_phone_number UNIQUE (phone_number);
+
+ALTER TABLE ticket_passengers
+ADD CONSTRAINT uk_ticket_passengers_ticket_id_passenger_ide UNIQUE (ticket_id, passenger_id);
