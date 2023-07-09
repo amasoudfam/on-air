da7854cd50a8ebb3e2853f6fb221542846c48257
masoud <masoud.aghdasifam@gmail.com>
2023-07-09 17:20:59 +0330

    Add generate PDF

--- server/handlers/ticket_pdf.go
+++ server/handlers/ticket_pdf.go
@@ -103,7 +103,7 @@ func generate_output(ticket models.Ticket) ([]byte, error) {
 		pdf.Cell(c3, 10, "Departure:")
 
 		pdf.SetFont(font, "BI", itemFontSize)
-		pdf.Cell(c4+10, 10, ticket.Flight.StartedAt.Format(time.RFC822))
+		pdf.Cell(c4+10, 10, ticket.Flight.StartedAt.Format(time.RFC1123))
 
 		pdf.Ln(lightSeparator)
 
@@ -117,7 +117,7 @@ func generate_output(ticket models.Ticket) ([]byte, error) {
 		pdf.Cell(c3, 10, "Arrival:")
 
 		pdf.SetFont(font, "BI", itemFontSize)
-		pdf.Cell(c4+10, 10, ticket.Flight.EndedAt.Format(time.RFC822))
+		pdf.Cell(c4+10, 10, ticket.Flight.EndedAt.Format(time.RFC1123))
 
 		pdf.Ln(lightSeparator)
 
@@ -147,38 +147,40 @@ func generate_output(ticket models.Ticket) ([]byte, error) {
 		ticketY += ticketHeight + 5
 		qrLocation += ticketHeight + 5
 	}
+
 	var buf bytes.Buffer
 	err := pdf.Output(&buf)
 	if err != nil {
 		return nil, err
 	}
+
 	return buf.Bytes(), nil
-	// return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
-	// err := pdf.OutputFileAndClose("ticket.pdf")
-	// if err != nil {
-	// 	panic(err)
-	// }
 }
 
 func (t *TicketPDF) Get(ctx echo.Context) error {
-	// userID, _ := strconv.Atoi(ctx.Get("id").(string))
-	userID := 2
+	userID, err := strconv.Atoi(ctx.Get("id").(string))
+	if err != nil {
+		return ctx.NoContent(http.StatusBadRequest)
+	}
+
 	ticketID, err := strconv.Atoi(ctx.QueryParam("ticket_id"))
 	if err != nil {
 		return ctx.NoContent(http.StatusBadRequest)
 	}
+
 	ticket, err := repository.GetTicket(t.DB, userID, ticketID)
 	if err != nil {
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
+
 	result, err := generate_output(ticket)
 	if err != nil {
 		return ctx.NoContent(http.StatusInternalServerError)
 	}
+
 	ctx.Response().Header().Set("Content-Type", "application/pdf")
 	ctx.Response().Header().Set("Content-Disposition", "attachment; filename=myfile.pdf")
 	ctx.Response().Header().Set("Content-Length", strconv.Itoa(len(result)))
 
 	return ctx.Blob(http.StatusOK, "application/pdf", result)
-	// return ctx.JSON(http.StatusOK, result)
 }
