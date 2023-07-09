8d2ed78dc52d8a855bb9c1b27c39ea6bea7a5e44
mintPC <masoud.aghdasifam@gmail.com>
2023-07-09 12:09:02 +0330

    Update datatype of times

---
+++ migrations/000004_flights_time_type.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE flights 
+ALTER COLUMN started_at TYPE date;
+ALTER TABLE flights 
+ALTER COLUMN ended_at TYPE date;
---
+++ migrations/000004_flights_time_type.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE flights 
+ALTER COLUMN started_at TYPE timestamp with time zone;
+ALTER TABLE flights 
+ALTER COLUMN ended_at TYPE timestamp with time zone;
