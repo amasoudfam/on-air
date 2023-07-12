7308f43b04928d011ef37233ef1ec4303ce5d49f
masoud <masoud.aghdasifam@gmail.com>
2023-07-12 19:43:28 +0330

    Merge of two ticket files

--- server/handlers/ticket_test.go
+++ server/handlers/ticket_test.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -22,7 +24,7 @@ import (
 	"gorm.io/gorm"
 )
 
-type TicketTestSuite struct {
+type GetTicketTestSuite struct {
 	suite.Suite
 	sqlMock  sqlmock.Sqlmock
 	e        *echo.Echo
@@ -31,7 +33,7 @@ type TicketTestSuite struct {
 	UserID   int
 }
 
-func (suite *TicketTestSuite) CallHandler() (*httptest.ResponseRecorder, error) {
+func (suite *GetTicketTestSuite) CallHandler() (*httptest.ResponseRecorder, error) {
 	req := httptest.NewRequest(http.MethodGet, suite.endpoint, nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	res := httptest.NewRecorder()
@@ -41,7 +43,7 @@ func (suite *TicketTestSuite) CallHandler() (*httptest.ResponseRecorder, error) 
 	return res, err
 }
 
-func (suite *TicketTestSuite) SetupSuite() {
+func (suite *GetTicketTestSuite) SetupSuite() {
 	mockDB, sqlMock, err := sqlmock.New()
 	if err != nil {
 		log.Fatal(err)
@@ -69,7 +71,7 @@ func (suite *TicketTestSuite) SetupSuite() {
 	suite.UserID = 1
 }
 
-func (suite *TicketTestSuite) TestGetTickets_Success() {
+func (suite *GetTicketTestSuite) TestGetTickets_Success() {
 	require := suite.Require()
 	expectedStatusCode := http.StatusOK
 	time := time.Now()
@@ -175,6 +177,169 @@ func (suite *TicketTestSuite) TestGetTickets_Success() {
 	require.Equal(expectedMsg, res.Body.String())
 }
 
-func TestTicket(t *testing.T) {
-	suite.Run(t, new(TicketTestSuite))
+type GetTicketPDFTestSuite struct {
+	suite.Suite
+	sqlMock  sqlmock.Sqlmock
+	e        *echo.Echo
+	endpoint string
+	ticket   *Ticket
+	UserID   int
+}
+
+func (suite *GetTicketPDFTestSuite) SetupSuite() {
+	mockDB, sqlMock, err := sqlmock.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: mockDB,
+	}))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	suite.sqlMock = sqlMock
+	suite.ticket = &Ticket{DB: db}
+	suite.e = echo.New()
+	suite.e.Validator = &utils.CustomValidator{Validator: validator.New()}
+	suite.endpoint = "/ticketPDF"
+	suite.UserID = 3
+}
+
+func (suite *GetTicketPDFTestSuite) CallGetHandler(ticket_id int) (*httptest.ResponseRecorder, error) {
+	req := httptest.NewRequest(http.MethodGet, suite.endpoint, nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	res := httptest.NewRecorder()
+	c := suite.e.NewContext(req, res)
+	c.Set("id", strconv.Itoa(suite.UserID))
+	if ticket_id > 0 {
+		c.QueryParams().Add("ticket_id", strconv.Itoa(ticket_id))
+	}
+	err := suite.ticket.GetPDF(c)
+	return res, err
+}
+
+func (suite *GetTicketPDFTestSuite) TestGetList_Success() {
+	require := suite.Require()
+	expectedStatusCode := http.StatusOK
+	time := time.Now()
+
+	tickets := []models.Ticket{
+		{
+			UnitPrice: 1200000,
+			Count:     1,
+			Status:    "complete",
+			User: models.User{
+				FirstName:   "user_fname",
+				LastName:    "user_lname",
+				Email:       "user@example.com",
+				PhoneNumber: "09122222222",
+			},
+			Flight: models.Flight{
+				Number:     "F102",
+				Airplane:   "F12",
+				Airline:    "Aseman",
+				StartedAt:  time,
+				FinishedAt: time,
+				FromCity: models.City{
+					Name: "Tehran",
+					Country: models.Country{
+						Name: "Iran",
+					},
+				},
+				ToCity: models.City{
+					Name: "Shiraz",
+					Country: models.Country{
+						Name: "Iran",
+					},
+				},
+			},
+			Passengers: []models.Passenger{
+				{
+					NationalCode: "2550000000",
+					FirstName:    "p1_fname",
+					LastName:     "p1_lname",
+					Gender:       "gmail",
+				},
+			},
+		},
+	}
+
+	tickets[0].ID = 1
+	tickets[0].CreatedAt = time
+
+	patch := monkey.Patch(repository.GetTicket, func(db *gorm.DB, userID int, ticketID int) (models.Ticket, error) {
+		return tickets[0], nil
+	})
+
+	defer patch.Unpatch()
+	res, err := suite.CallGetHandler(2)
+	require.NoError(err)
+	require.Equal(expectedStatusCode, res.Code)
+}
+
+func (suite *GetTicketPDFTestSuite) TestGetList_Failure_InvalidTicketID() {
+	require := suite.Require()
+	expectedStatusCode := http.StatusBadRequest
+	expectedBody := "\"Invalid ticket_id\"\n"
+
+	patch := monkey.Patch(repository.GetTicket, func(db *gorm.DB, userID int, ticketID int) (models.Ticket, error) {
+		return models.Ticket{}, nil
+	})
+	defer patch.Unpatch()
+
+	res, err := suite.CallGetHandler(0)
+	require.NoError(err)
+	body, _ := io.ReadAll(res.Body)
+	require.Equal(expectedBody, string(body))
+	require.Equal(expectedStatusCode, res.Code)
+}
+
+func (suite *GetTicketPDFTestSuite) TestGetList_Failure_InternalError() {
+	require := suite.Require()
+	expectedStatusCode := http.StatusInternalServerError
+	expectedBody := "\"Internal error\"\n"
+
+	patch := monkey.Patch(repository.GetTicket, func(db *gorm.DB, userID int, ticketID int) (models.Ticket, error) {
+		return models.Ticket{}, errors.New("Internal error")
+	})
+	defer patch.Unpatch()
+
+	res, err := suite.CallGetHandler(2)
+	require.NoError(err)
+	body, _ := io.ReadAll(res.Body)
+	require.Equal(expectedBody, string(body))
+	require.Equal(expectedStatusCode, res.Code)
+}
+
+func (suite *GetTicketPDFTestSuite) TestGetList_Failure_GenerateOutput() {
+	require := suite.Require()
+	expectedStatusCode := http.StatusInternalServerError
+	expectedBody := "\"Internal error\"\n"
+
+	patch1 := monkey.Patch(repository.GetTicket, func(db *gorm.DB, userID int, ticketID int) (models.Ticket, error) {
+		return models.Ticket{}, nil
+	})
+	defer patch1.Unpatch()
+
+	patch2 := monkey.Patch(utils.GeneratePDF, func(ticket models.Ticket) ([]byte, error) {
+		return nil, errors.New("Internal error")
+	})
+	defer patch2.Unpatch()
+
+	res, err := suite.CallGetHandler(2)
+	require.NoError(err)
+	body, _ := io.ReadAll(res.Body)
+	require.Equal(expectedBody, string(body))
+	require.Equal(expectedStatusCode, res.Code)
+}
+
+func TestGetTicket(t *testing.T) {
+	suite.Run(t, new(GetTicketTestSuite))
+}
+
+func TestGetTicketPDF(t *testing.T) {
+	suite.Run(t, new(GetTicketPDFTestSuite))
 }
