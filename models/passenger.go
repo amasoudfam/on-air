bea6ed8a760f8804472017c9adb217241249f2f6
masoud <masoud.aghdasifam@gmail.com>
2023-07-14 12:06:40 +0330

    Add unique constraint

--- models/passenger.go
+++ models/passenger.go
@@ -6,8 +6,8 @@ import (
 
 type Passenger struct {
 	gorm.Model
-	UserID       uint
-	NationalCode string `gorm:"type:varchar(10)"`
+	UserID       uint   `gorm:"uniqueIndex:idx_name_location"`
+	NationalCode string `gorm:"type:varchar(10);uniqueIndex:idx_name_location"`
 	FirstName    string `gorm:"type:varchar(50)"`
 	LastName     string `gorm:"type:varchar(50)"`
 	Gender       string `gorm:"type:varchar(5)"`
